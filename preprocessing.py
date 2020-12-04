36f7b741faf117c79dfba50d961925456a14e84c
laurenanders20 <57648320+laurenanders20@users.noreply.github.com>
2020-12-04 13:16:18 -0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,8 @@ import h5py
 import numpy as np
 from scipy.io import loadmat
 
-# sys.argv should be file paths to train.mat, valid.mat, test.mat
-
 def load_files(train_file, valid_file, test_file):
+    print("loading training data")
     with h5py.File(train_file, 'r') as train_data:
         train_labels = np.transpose(train_data['traindata'], (1, 0))
 
@@ -14,31 +13,36 @@ def load_files(train_file, valid_file, test_file):
         for i in range(int(4400000/1000)):
             train_inputs[i*1000:(i+1)*1000] = np.transpose(np.argmax(traindata[:, :, i*1000:(i+1)*1000], axis=1))
 
+    print("loading validation data")
     valid_data = loadmat(valid_file)
 
     valid_labels = valid_data['validdata']
     valid_inputs = np.argmax(valid_data['validxdata'], axis = 1)
 
+    print("loading testing data")
     test_data = loadmat(test_file)
 
     test_labels = test_data['testdata']
     test_inputs = np.argmax(test_data['testxdata'], axis = 1)
-
     return train_inputs, train_labels, valid_inputs, valid_labels, test_inputs, test_labels
 
 def get_kmers(inputs, labels, dna_dict, len_kmer):
-    kmers = np.empty((len(inputs), int(len(inputs[0])/len_kmer + 1)), dtype=object)
+    kmers = np.empty((len(inputs), 3), dtype=object)
 
     for i in range(len(inputs)):
-        curr_index = 0
-        for j in range(int(len(inputs[0])/len_kmer)):
-            kmer = ''
-            for k in range(len_kmer):
-                kmer += dna_dict[inputs[i, curr_index]]
-                curr_index += 1
-            kmers[i, j] = kmer
-        kmers[i, -1] = labels[i]
+        # just to track time in big files -- can delete this
+        if i % 10000 == 0:
+            print(i/len(inputs))
 
+        curr_index = 0
+        kmers[i, 0] = '' #kmers separated by spaces
+        while curr_index < len(inputs[0]):
+            for k in range(len_kmer):
+                kmers[i, 0] += dna_dict[inputs[i, curr_index]]
+                curr_index += 1
+            kmers[i, 0] += " "
+        kmers[i, 1] = 1 # fake label
+        kmers[i, 2] = labels[i] # real label
     return kmers
 
 k = 4
@@ -49,9 +53,16 @@ dna_dict = {
     3: "G"
 }
 
+train_inputs, train_labels, valid_inputs, valid_labels, test_inputs, test_labels = load_files('train.mat', 'valid.mat', 'test.mat')
 
-train_inputs, train_labels, valid_inputs, valid_labels, test_inputs, test_labels = load_files(sys.argv[1], sys.argv[2], sys.argv[3])
-
+print("starting validation kmers")
 valid_kmers = get_kmers(valid_inputs, valid_labels, dna_dict, k)
+np.savetxt('valid_4.gz', valid_kmers, fmt='%s', delimiter='\t')
+
+print("starting testint kmers")
 test_kmers = get_kmers(test_inputs, test_labels, dna_dict, k)
+np.savetxt('test_4.gz', test_kmers, fmt='%s', delimiter='\t')
+
+print("starting training kmers")
 train_kmers = get_kmers(train_inputs, train_labels, dna_dict, k)
+np.savetxt('train_4.gz', train_kmers, fmt='%s', delimiter='\t')
