f4d9f5912817adea36c3c52b3ebc51c14d513e00
shubhang8 <49883050+shubhang8@users.noreply.github.com>
2020-12-11 21:38:50 -0800

    Update deepssl.py

--- DNABert/examples/deepssl.py
+++ DNABert/examples/deepssl.py
@@ -1,5 +1,5 @@
 import sys
-sys.settrace
+sys.path
 # coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
